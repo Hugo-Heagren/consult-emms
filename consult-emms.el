7fc668bf7e730e76d514b337ff5c7a75db57b997
Hugo Heagren <hugo@heagren.com>
2021-11-14 20:53:57 +0000

    Initial Commit

---
+++ consult-emms.el
@@ -0,0 +1,15 @@
+;;; consult-emms.el --- Consult interface to EMMS
+
+;; Author: Hugo Heagren <hugo@heagren.com>
+;; Version: 0.1
+;; Package-Requires: ((consult) (emms))
+;; Keywords: consult, emms
+
+;;; Code:
+
+(require 'consult)
+(require 'emms)
+
+(provide 'consult-emms)
+
+;;; consult-emms.el ends here
