f5dc3b292df7b47bcce530375295c68056f94ea4
Hugo Heagren <hugo@heagren.com>
2021-12-22 13:54:02 +0000

    Skeleton file for embark support

---
+++ consult-emms-embark.el
@@ -0,0 +1,15 @@
+;;; consult-emms-embark.el --- Embark actions for consult-emms
+
+;; Author: Hugo Heagren <hugo@heagren.com>
+;; Version: 0.1
+;; Package-Requires: ((consult-emms) (embark))
+;; Keywords: consult, emms, embark
+
+;;; Code:
+
+(require 'consult-emms)
+(require 'embark)
+
+(provide 'consult-emms-embark)
+
+;;; consult-emms-embark.el ends here
