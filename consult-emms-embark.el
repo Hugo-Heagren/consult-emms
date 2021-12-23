00350efef5207a2f41be8e97ad944d3a182a7d0c
Hugo Heagren <hugo@heagren.com>
2021-12-23 12:10:31 +0000

    embark: add action to goto track's artist
    Very similar to previous action to go to track's album.

--- consult-emms-embark.el
+++ consult-emms-embark.el
@@ -30,10 +30,23 @@ Selected track is added to the current playlist."
 			       :category 'track)))
     (consult-emms--add-track-current-playlist track)))
 
+(defun consult-emms-embark--track-goto-artist (track-name)
+  "Select a track by TRACK-NAME's artist.
+
+Selected track is added to the current playlist."
+  (let* ((artist (consult-emms--track-name-get track-name 'info-artist))
+	 (tracks (mapcar #'consult-emms--propertize-track-title
+			 (consult-emms--get-artist-tracks artist)))
+	 (track (consult--read tracks
+			       :prompt (format "%s: " artist)
+			       :category 'track)))
+    (consult-emms--add-track-current-playlist track)))
+
 (embark-define-keymap consult-emms-embark-track-actions
   "Keymap for actions on tracks in `consult-emms'."
   ("p" '("Add to playlist" . consult-emms-embark--add-track-playlist))
-  ("b" '("Goto album" . consult-emms-embark--track-goto-album)))
+  ("b" '("Goto album" . consult-emms-embark--track-goto-album))
+  ("a" '("Goto artist" . consult-emms-embark--track-goto-artist)))
 
 (add-to-list 'embark-keymap-alist '(track . consult-emms-embark-track-actions))
 
