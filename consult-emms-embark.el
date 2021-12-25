c5fed51a37d4d64b18bcf589805c4ffc7cae23cc
Hugo Heagren <hugo@heagren.com>
2021-12-25 19:14:17 +0000

    embark: add action to clear tracks from playlist

--- consult-emms-embark.el
+++ consult-emms-embark.el
@@ -120,7 +120,8 @@ Selected track is added to the current playlist."
 (embark-define-keymap consult-emms-embark-playlist-actions
   "Keymap for actions on playlists in `consult-emms'."
   ("W" '("Write to file" . consult-emms-embark--write-playlist))
-  ("k" '("Kill playlist" . consult-emms-embark--kill-playlist)))
+  ("k" '("Kill playlist" . consult-emms-embark--kill-playlist))
+  ("c" '("Clear playlist" . consult-emms-embark--clear-playlist)))
 
 (add-to-list 'embark-keymap-alist '(playlist . consult-emms-embark-playlist-actions))
 
